f8ef80ecaddca528aa77d7360029641383e66d7a
dmac <dmac100@users.noreply.github.com>
2016-09-04 19:08:33 +0100

    Added menubar and adjusted size and title.

--- src/Workbook.java
+++ src/Workbook.java
@@ -3,6 +3,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 import view.CellList;
+import view.MenuBuilder;
 
 public class Workbook {
 	private final Shell shell;
@@ -13,6 +14,17 @@ public class Workbook {
 		shell.setLayout(new FillLayout());
 		
 		CellList promptsList = new CellList(shell);
+		
+		createMenuBar(shell);
+	}
+	
+	private void createMenuBar(final Shell shell) {
+		MenuBuilder menuBuilder = new MenuBuilder(shell);
+		
+		menuBuilder.addMenu("&File")
+			.addItem("E&xit\tCtrl+Q").addSelectionListener(() -> shell.dispose());
+		
+		menuBuilder.build();
 	}
 	
 	public static void main(String[] args) {
@@ -22,7 +34,8 @@ public class Workbook {
 
 		Workbook main = new Workbook(shell);
 		
-		shell.setSize(400, 300);
+		shell.setText("Workbook");
+		shell.setSize(900, 600);
 		shell.open();
 
 		while(!shell.isDisposed()) {
