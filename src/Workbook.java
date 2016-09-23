7c745d098c164071e0c6f114f2242fcd1b2cfb92
dmac <dmac100@users.noreply.github.com>
2016-09-23 03:00:29 +0100

    Fixed view created with new canvas menu item.

--- src/Workbook.java
+++ src/Workbook.java
@@ -47,7 +47,7 @@ public class Workbook {
 			.addItem("New Console").addSelectionListener(() -> viewFactory.addConsole())
 			.addItem("New Worksheet").addSelectionListener(() -> viewFactory.addWorksheet())
 			.addItem("New Script").addSelectionListener(() -> viewFactory.addScript())
-			.addItem("New Canvas").addSelectionListener(() -> viewFactory.addScript())
+			.addItem("New Canvas").addSelectionListener(() -> viewFactory.addCanvasView())
 			.addSeparator()
 			.addItem("New String Editor...").addSelectionListener(() -> getExpression(expression -> viewFactory.addStringEditor(expression)))
 			.addItem("New Table Editor...").addSelectionListener(() -> getExpression(expression -> viewFactory.addTableEditor(expression)))
