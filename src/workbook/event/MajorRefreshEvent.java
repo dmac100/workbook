4e4d47d2342a5c7b117ce6bceb8fd2b064890487
dmac <dmac100@users.noreply.github.com>
2016-12-13 22:01:54 +0000

    Corrected comment.

--- src/workbook/event/MajorRefreshEvent.java
+++ src/workbook/event/MajorRefreshEvent.java
@@ -1,7 +1,7 @@
 package workbook.event;
 
 /**
- * An event dispatches when a major refresh occurs. This should cauase all editors to
+ * An event dispatched when a major refresh occurs. This should cauase all editors to
  * refresh their values.
  */
 public class MajorRefreshEvent {
--- src/workbook/event/MinorRefreshEvent.java
+++ src/workbook/event/MinorRefreshEvent.java
@@ -1,7 +1,7 @@
 package workbook.event;
 
 /**
- * An event dispatches when a minor refresh occurs. This should cauase editors without
+ * An event dispatched when a minor refresh occurs. This should cauase editors without
  * side-effects to refresh their values.
  */
 public class MinorRefreshEvent {
