4c5c21b8e4a86bf569de55b48c4823f7de4f0b79
dmac <dmac100@users.noreply.github.com>
2016-12-22 22:25:41 +0000

    Added MainController serialization tests.

---
+++ test/workbook/controller/MainControllerTest.java
@@ -0,0 +1,52 @@
+package workbook.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import org.jdom2.Element;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.eventbus.EventBus;
+
+import workbook.model.Model;
+import workbook.script.JavascriptEngine;
+import workbook.script.ScriptController;
+
+public class MainControllerTest {
+	private MainController mainController;
+	private ScriptController scriptController;
+	
+	@Before
+	public void before() {
+		mainController = new MainController(mock(EventBus.class), mock(Model.class));
+		scriptController = mainController.getScriptController();
+		mainController.registerEngine("Javascript", new JavascriptEngine());
+		mainController.setEngine("Javascript");
+	}
+	
+	@Test
+	public void serializationRoundTrip() throws Exception {
+		Element root = new Element("root");
+		
+		mainController.addVariable("x", 1);
+		mainController.serialize(root);
+		mainController.addVariable("x", 2);
+		
+		mainController.deserialize(root);
+		Object result = scriptController.eval("x").get();
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void serializationRoundTrip_keepSystem() throws Exception {
+		mainController.addVariable("system", 3);
+		
+		Element root = new Element("root");
+		mainController.serialize(root);
+		mainController.deserialize(root);
+		
+		Object result = scriptController.eval("system").get();
+		assertEquals(3, result);
+	}
+}
