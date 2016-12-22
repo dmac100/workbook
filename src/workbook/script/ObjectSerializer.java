7adbb1e92010d62acbec41cf688ea345e98d8194
dmac <dmac100@users.noreply.github.com>
2016-12-22 21:55:57 +0000

    Only serialize serializable objects.

--- src/workbook/script/ObjectSerializer.java
+++ src/workbook/script/ObjectSerializer.java
@@ -1,25 +1,56 @@
 package workbook.script;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.mapper.CannotResolveClassException;
+import com.thoughtworks.xstream.mapper.MapperWrapper;
 
 /**
  * Serializes objects to and from Strings for saving to a file.
  */
 public class ObjectSerializer {
+	private final XStream xstream;
+
+	public ObjectSerializer() {
+		xstream = new XStream(new DomDriver()) {
+			protected MapperWrapper wrapMapper(MapperWrapper next) {
+				return new MapperWrapper(next) {
+					public boolean shouldSerializeMember(Class definedIn, String fieldName) {
+						try {
+							return definedIn != Object.class || realClass(fieldName) != null;
+						} catch(CannotResolveClassException e) {
+							return false;
+						}
+					}
+				};
+			}
+		};
+		
+		xstream.ignoreUnknownElements();
+	}
+
 	/**
 	 * Returns map serialized to a String.
 	 */
 	public String serialize(Map<String, Object> map) throws IOException {
-		return new XStream().toXML(map);
+		Map<String, Object> serializable = new HashMap<>();
+		map.forEach((k, v) -> {
+			if(v instanceof Serializable) {
+				serializable.put(k, v);
+			}
+		});
+		return xstream.toXML(map);
 	}
-	
+
 	/**
 	 * Returns map deserialized from a String.
 	 */
 	public Map<String, Object> deserialize(String data) throws IOException, ClassNotFoundException {
-		return (Map<String, Object>) new XStream().fromXML(data);
+		return (Map<String, Object>) xstream.fromXML(data);
 	}
 }
