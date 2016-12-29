f9ae55edb16acd027bde421f6344d20beaf50742
dmac <dmac100@users.noreply.github.com>
2016-12-29 06:47:11 +0000

    Added execDisplay util method.

--- src/workbook/util/SwtUtil.java
+++ src/workbook/util/SwtUtil.java
@@ -1,9 +1,13 @@
 package workbook.util;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 
 public class SwtUtil {
 	public static void forEachDescendant(Composite parent, Consumer<Control> consumer) {
@@ -12,6 +16,25 @@ public class SwtUtil {
 			if(child instanceof Composite) {
 				forEachDescendant((Composite) child, consumer);
 			}
+		}
+	}
+	
+	/**
+	 * Runs a callable on the display thread, and returns the result.
+	 */
+	public static <T> T execDisplay(Callable<T> callable) {
+		try {
+			CompletableFuture<T> future = new CompletableFuture<>();
+			Display.getDefault().asyncExec(() -> {
+				try {
+					future.complete(callable.call());
+				} catch(Exception e) {
+					future.completeExceptionally(e);
+				}
+			});
+			return future.get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException("Error executing on display thread", e);
 		}
 	}
 }
